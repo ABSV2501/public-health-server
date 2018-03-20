f5dca275ab2f472cf22a7863fa433393e17a817b
bhavya <bhavya2b@gmail.com>
2018-03-21 03:23:18 +0530

    govt portal
    Signed-off-by: bhavya <bhavya2b@gmail.com>

--- routes/api/govt.js
+++ routes/api/govt.js
@@ -1,10 +1,21 @@
 const route = require("express").Router();
 
-route.get("/",(res,req) => {
+let Storage = multer.diskStorage({
+    destination: './public_html/csv',
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+let upload = multer({storage: Storage});
+route.get("/",(req,res) => {
     res.redirect("/govt.html");
 });
 
+route.post('/infrastructure',upload.single('csv'),(req,res)=>{
 
+});
+route.post('/rating',upload.single('csv'),(req,res)=>{
 
+});
 
 module.exports = route;
